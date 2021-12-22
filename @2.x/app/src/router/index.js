ac30525158d8075eb5459f74ef2a2d2f9742b079
Fly <>
2021-12-22 19:57:00 +0800

    feat(/router/index.js): 重写 VueRouter push 和 replace 方法

--- @2.x/app/src/router/index.js
+++ @2.x/app/src/router/index.js
@@ -3,12 +3,31 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 // Vue 使用路由
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-import Home from "@/pages/Home"
-import Search from "@/pages/Search"
+import Home from "@/pages/Home";
+import Search from "@/pages/Search";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
+
+let originPush = VueRouter.prototype.push;
+let originReplace = VueRouter.prototype.replace;
+
+VueRouter.prototype.push = function (
+  location,
+  resolve = () => {},
+  reject = () => {}
+) {
+  originPush.call(this, location, resolve, reject);
+};
+
+VueRouter.prototype.replace = function (
+  location,
+  resolve = () => {},
+  reject = () => {}
+) {
+  originReplace.call(this, location, resolve, reject);
+};
 
 export default new VueRouter({
   routes: [
