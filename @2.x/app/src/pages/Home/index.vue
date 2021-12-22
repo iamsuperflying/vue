3b1ae67ff7e7aa279d59b203b03a72b12fd0d788
Fly <>
2021-12-22 20:19:07 +0800

    feat(/Home): 添加 TypeNav

--- @2.x/app/src/pages/Home/index.vue
+++ @2.x/app/src/pages/Home/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     首页...
+    <TypeNav></TypeNav>
   </div>
 </template>
 
 <script>
+
+import TypeNav from "@/pages/Home/TypeNav"
+
 export default {
+
+  name: "Home",
+  components: {
+    TypeNav
+  }
 
 }
 </script>
